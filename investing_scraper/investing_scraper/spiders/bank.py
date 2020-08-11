bffa4ede98aa0896fe56967f6a16bfff175687a5
Aseanseen <sean.lim.110@gmail.com>
2020-08-11 13:44:30 +0800

    final working version

--- investing_scraper/investing_scraper/spiders/bank.py
+++ investing_scraper/investing_scraper/spiders/bank.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 class InvestSpider(scrapy.Spider):
-    name = "my-investing-spider"
+    name = "my-bank-investing-spider"
     urls=[]
     def __init__(self,inv_link='',yahoo_link='',driver=''):
         self.urls.append(f'{yahoo_link}')
@@ -20,7 +20,7 @@ class InvestSpider(scrapy.Spider):
     def parse_income(self,response):
         #Use scrapy for quarterly
         quarterly_net_income = response.xpath('//*[text() = "Net Income"]/../../td[position()=2 or position()=3 or position()=4 or position()=5]/text()').getall()
-        quarterly_revenue = response.xpath('//*[text() = "Total Revenue"]/../../td[position()=2 or position()=3 or position()=4 or position()=5]/text()').getall()
+        #quarterly_revenue = response.xpath('//*[text() = "Total Revenue"]/../../td[position()=2 or position()=3 or position()=4 or position()=5]/text()').getall()
         #Click the button
         annual_button = self.driver.find_element_by_link_text('Annual')
         self.driver.execute_script('arguments[0].click()',annual_button)
@@ -33,12 +33,14 @@ class InvestSpider(scrapy.Spider):
         #time.sleep(1)
         #Use selenium for annual
         annualy_net_income = []
-        annualy_revenue = []
+        #annualy_revenue = []
         for i in range(2,6):
             income = self.driver.find_element(By.XPATH,'//*[text() = "Net Income"]/../../td[position()='+str(i)+']').text
-            revenue = self.driver.find_element(By.XPATH,'//*[text() = "Total Revenue"]/../../td[position()='+str(i)+']').text
+            #revenue = self.driver.find_element(By.XPATH,'//*[text() = "Total Revenue"]/../../td[position()='+str(i)+']').text
             annualy_net_income.append(income)
-            annualy_revenue.append(revenue)
+            #annualy_revenue.append(revenue)
+        quarterly_revenue = []
+        annualy_revenue = []
         results_dict = {
             'quarterly_net_income': quarterly_net_income,
             'annualy_net_income': annualy_net_income,
@@ -54,6 +56,7 @@ class InvestSpider(scrapy.Spider):
         self.driver.execute_script('arguments[0].click()',annual_button)
         wait = WebDriverWait(self.driver,1)
         wait.until(
+        #ec.element_to_be_clickable((By.XPATH,"//*[text() = 'Quarterly']"))
         ec.visibility_of_element_located((By.XPATH,"//*[text() = 'Total Equity']"))
         )
         #time.sleep(1)
@@ -130,6 +133,23 @@ class InvestSpider(scrapy.Spider):
         #Take data from Income
         net_income = self.driver.find_element(By.XPATH,"//*[contains(@title,'Net income')]/../../div[position()=2]").text
         income_tax = self.driver.find_element(By.XPATH,"//*[contains(@title,'Income tax expense')]/../../div[position()=2]").text
+        
+        quarterly_revenue = []
+        annualy_revenue = []
+        for i in range(3,7):
+            rev = self.driver.find_element(By.XPATH,"//*[contains(@title,'Total revenue')]/../../div[position()="+str(i)+']').text
+            annualy_revenue.append(rev)
+        
+        #Click quarterly
+        quarterly_button = self.driver.find_element(By.XPATH,"//*[span/text()='Quarterly']/..")
+        self.driver.execute_script('arguments[0].click()',quarterly_button)
+        time.sleep(1)
+        for i in range(3,7):
+            rev = self.driver.find_element(By.XPATH,"//*[contains(@title,'Total revenue')]/../../div[position()="+str(i)+']').text
+            quarterly_revenue.append(rev)
+        results_dict['quarterly_revenue'] = quarterly_revenue
+        results_dict['annualy_revenue'] = annualy_revenue
+
         #Click Cash flow
         cash_flow_button = self.driver.find_element_by_link_text('Cash flow')
         self.driver.execute_script('arguments[0].click()',cash_flow_button)
@@ -140,17 +160,17 @@ class InvestSpider(scrapy.Spider):
         )
         time.sleep(1)
         #Take data from Cash flow
-        amt_receivable_ttm = self.driver.find_element(By.XPATH,"//*[contains(@title,'Accounts receivable')]/../../div[position()=2]").text
-        amt_receivable_1 = self.driver.find_element(By.XPATH,"//*[contains(@title,'Accounts receivable')]/../../div[position()=3]").text
-        amt_receivable_2 = self.driver.find_element(By.XPATH,"//*[contains(@title,'Accounts receivable')]/../../div[position()=4]").text 
+        #amt_receivable_ttm = self.driver.find_element(By.XPATH,"//*[contains(@title,'Accounts receivable')]/../../div[position()=2]").text
+        #amt_receivable_1 = self.driver.find_element(By.XPATH,"//*[contains(@title,'Accounts receivable')]/../../div[position()=3]").text
+        #amt_receivable_2 = self.driver.find_element(By.XPATH,"//*[contains(@title,'Accounts receivable')]/../../div[position()=4]").text 
         #Accounts payable choice
-        amt_payable = self.driver.find_element(By.XPATH,"//*[contains(@title,'Accounts payable')]/../../div[position()=2]").text
-        capital_expen = self.driver.find_element(By.XPATH,"//*[contains(@title,'Capital expenditure')]/../../div[position()=2]").text
-        free_cash_flow = self.driver.find_elements(By.XPATH,"//*[contains(@title,'Free cash flow')]/../../div[position()=2]")
+        #amt_payable = self.driver.find_element(By.XPATH,"//*[contains(@title,'Accounts payable')]/../../div[position()=2]").text
+        capital_expen = self.driver.find_element(By.XPATH,"//*[contains(@title,'Capital expenditure')]/../../div[position()=3]").text
+        free_cash_flow = self.driver.find_elements(By.XPATH,"//*[contains(@title,'Free cash flow')]/../../div[position()=3]")
         free_cash_flow = free_cash_flow[1].text
-        depre_amor = self.driver.find_element(By.XPATH,"//*[contains(@title,'Depreciation & amortisation')]/../../div[position()=2]").text
+        depre_amor = self.driver.find_element(By.XPATH,"//*[contains(@title,'Depreciation & amortisation')]/../../div[position()=3]").text
  
-        #Accounts payable choice, try not to use, not accurate
+        #Accounts payable choice
         #Click Balance Sheet
         #balance_button = self.driver.find_element_by_link_text('Balance sheet')
         #self.driver.execute_script('arguments[0].click()',balance_button)
@@ -159,12 +179,16 @@ class InvestSpider(scrapy.Spider):
         #wait.until(
         #ec.visibility_of_element_located((By.XPATH,"//*[contains(@title,'Total cash')]"))
         #)
-        #time.sleep(1)
         #amt_payable = self.driver.find_element(By.XPATH,"//*[contains(@title,'Accounts payable')]/../../div[position()=2]").text
         #amt_receivable_ttm = self.driver.find_element(By.XPATH,"//*[contains(@title,'Net receivable')]/../../div[position()=2]").text
         #amt_receivable_1 = self.driver.find_element(By.XPATH,"//*[contains(@title,'Net receivable')]/../../div[position()=3]").text
         #amt_receivable_2 = self.driver.find_element(By.XPATH,"//*[contains(@title,'Net receivable')]/../../div[position()=4]").text 
-
+        
+        #Note: these data appear to not be available for DBS, hence the '0'
+        amt_receivable_ttm = '0'
+        amt_receivable_1 = '0'
+        amt_receivable_2 = '0'
+        amt_payable = '0'
         #Corrects the values to proper numbers
         l = [net_income,income_tax,depre_amor,amt_receivable_ttm,amt_receivable_1,amt_receivable_2,amt_payable,capital_expen,free_cash_flow]
         for n in range(len(l)):
@@ -183,11 +207,11 @@ class InvestSpider(scrapy.Spider):
         peg = self.driver.find_element(By.XPATH,"//*[span/text()='PEG Ratio (5 yr expected)']/../td[position()=2]").text
         psr = self.driver.find_element(By.XPATH,"//*[span/text()='Price/sales']/../td[position()=2]").text
 
-        roe = self.driver.find_element(By.XPATH,"//*[span/text()='Return on assets']/../td[position()=2]").text
-        roic = self.driver.find_element(By.XPATH,"//*[span/text()='Return on equity']/../td[position()=2]").text
+        roa = self.driver.find_element(By.XPATH,"//*[span/text()='Return on assets']/../td[position()=2]").text
+        roe = self.driver.find_element(By.XPATH,"//*[span/text()='Return on equity']/../td[position()=2]").text
         payout_ratio = self.driver.find_element(By.XPATH,"//*[span/text()='Payout ratio']/../td[position()=2]").text
         profit_margin = self.driver.find_element(By.XPATH,"//*[span/text()='Profit margin']/../td[position()=2]").text
-        levered_free_cash = self.driver.find_element(By.XPATH,"//*[span/text()='Levered free cash flow']/../td[position()=2]").text
+        levered_free_cash = self.driver.find_element(By.XPATH,"//*[span/text()='Levered free cash flow']/../td[position()=2]").text 
         num_shares = self.driver.find_element(By.XPATH,"//*[span/text()='Shares outstanding']/../td[position()=2]").text
         if (eps == '-' or eps == 'N/A' or eps == '0'):
             eps = '0'
@@ -195,7 +219,6 @@ class InvestSpider(scrapy.Spider):
             levered_free_cash = '0'
         if (peg == '-' or peg == 'N/A' or peg == '0'):
             peg = '0'
-
         results_dict['ttm_eps,peg,ttm_levered_cash'] = [eps,peg,levered_free_cash]
         results_dict['ttm_net_income,ttm_income_tax,ttm_depre'] = [l[0],l[1],l[2]]
         results_dict['ttm_receivable,receivable_1,receivable_2,ttm_payable'] = [l[3],l[4],l[5],l[6]]
